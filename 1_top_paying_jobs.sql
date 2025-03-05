61fb3ffc0be4b653bd91491ae5d46aca4475d331
Nithyasri Sundar <nithyasrisundar1998@gmail.com>
2025-03-05 16:07:23 -0500

    Update 1_top_paying_jobs.sql

--- 1_top_paying_jobs.sql
+++ 1_top_paying_jobs.sql
@@ -2,7 +2,7 @@
 Question: What are the top-pauing data analyst jobs?
 -Identify the top 10 highest-paying Data Analyst roles that are available remotely.
 -Focuses on job postings with specified salaries (remove nulls).
--Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment.
+-Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
 */
 SELECT
     job_postings_fact.company_id,
